7f31dba10948534eed5f82648496d26cfd717fa7
bhavana522 <67970804+bhavana522@users.noreply.github.com>
2021-02-05 09:51:59 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Todoform from './components/Todoform';
+import Todolist from './components/Todolist';
+
+function App() {
+  return (
+    <div className="todo-app">
+       <Todolist />
+    </div>
+  );
+}
+
+export default App;
